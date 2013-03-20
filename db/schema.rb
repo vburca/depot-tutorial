fddd5d460b1abd3e3f32431ed360b342788e6056
Vlad Burca <vladburca2005@gmail.com>
2013-03-19 23:34:18 -0400

    Combined line items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320031529) do
+ActiveRecord::Schema.define(:version => 20130320032315) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
